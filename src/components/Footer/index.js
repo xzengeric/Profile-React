11d5f58303f0de038cadcc9793ae8e58af4bd6dd
Eric Zeng <38875467+xzengeric@users.noreply.github.com>
2020-05-02 22:05:41 -0700

    done

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>React profile</span>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Protfolio.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import Jumbotron from "react-bootstrap/jumbotron"
+import "../components/Style/style.css"
+import CodeQuiz from "../components/assets/images/CodeQuiz.PNG"
+import passwordGenerator from "../components/assets/images/passwordGenerator.PNG"
+import project1 from "../components/assets/images/project1.PNG"
+function Protfolio() {
+        return (
+            <div>
+
+           
+            <Jumbotron>
+
+            <div className="Protfolio-heading">
+                <h1>Protfolio</h1>
+                <p>Please Click to explore more!</p>
+            </div>
+
+            
+                <a href="https://xzengeric.github.io/codeQuiz/index.html"> <img src={CodeQuiz} alt="..."/>
+                </a>
+             
+                <a href="https://xzengeric.github.io/Password-Generator/"> <img
+                        src={passwordGenerator} alt="..."/>
+                </a>
+            
+               
+                <a href="https://xzengeric.github.io/Project1/project1.html"> <img 
+                        src={project1} alt="..."/>
+                </a>
+              
+
+
+        </Jumbotron>
+
+        
+
+        </div>
+    );
+  }
+  export default Protfolio;
